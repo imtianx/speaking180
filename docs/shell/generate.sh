5a273f18128cec08bc10b7b71d034adcf0d77119
imtianx <imtianx@gmail.com>
2019-06-14 13:30:51 +0800

    update generate script;
    Signed-off-by: imtianx <imtianx@gmail.com>

--- docs/shell/generate.sh
+++ docs/shell/generate.sh
@@ -1,29 +1,48 @@
 #! /bin/bash
 
+# config file
 templeteFileName="note.md.template"
 sidebarFineName="_sidebar.md"
+configFile="config.json"
 
-no="NO"
-titleEn="TITLE_EN"
-titleZh="TITLE_EN"
+# templete params
+templete_no="NO"
+templete_title_en="TITLE_EN"
+templete_title_zh="TITLE_ZH"
 
-temp=""
-formateNo=""
 
-for i in {3..180}
-do
-  if [ ${i} -lt 10 ];
-  then
-    formateNo="00${i}"
-  elif [ ${i} -lt 100 ];
-  then
-    formateNo="0${i}"
-  else
-    formateNo="${i}"
-  fi
-  temp="${formateNo}.md"
-  cp ${templeteFileName} ${temp};
-  sed -i "" "s/${no}/${formateNo}/g" ${temp};
-  echo "  * [${formateNo}.](note/${formateNo}.md)" >> ${sidebarFineName};
+# init sidebar file
+echo "* **目录**" > ${sidebarFineName};
+
+newFile=""
+
+# json config data ,using jq tools 
+data=`cat ${configFile} | jq '.' `
+
+length=`cat ${configFile} | jq 'length' `
+
+for index in `seq 1 ${length}`
+do 
+
+  # get fields
+  no=`echo $data | jq -r ".[$index-1].no" `
+  title_en=`echo $data | jq -r ".[$index-1].title_en" `
+  title_zh=`echo $data | jq -r ".[$index-1].title_zh" `
+
+  # copy file 
+  newFile="${no}.md"
+  cp ${templeteFileName} ${newFile};
+
+  # modify templete
+  sed -i "" "s/${templete_no}/${no}/g;s/${templete_title_en}/${title_en}/g;s/${templete_title_zh}/${title_zh}/g" ${newFile};
+
+  # add file to sidebar
+  echo "  * [${no}.${title_zh}](note/${no}.md)" >> ${sidebarFineName};
+
+  echo "${no} file generated!"
+  
 done
+
+echo "---------------------all generated task exectue success !"
+
 
